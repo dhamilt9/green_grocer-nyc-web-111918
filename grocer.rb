9133fdb19e632975b68259044eae149b2c8d680e
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-05 21:44:06 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -7,7 +7,10 @@ cart=[
 def consolidate_cart(cart)
   consolidatedcart={}
   cart.each do |item|
-    puts item.keys[0]
+    name=item.keys[0]
+    price=item.values[0][:price]
+    clearance=item.values[0][:clearance]
+    puts "Name=#{name}, price=#{price}, clearance=#{clearance}"
   end
   return consolidatedcart
 end
