8e45b1dcf5df6b1d740e3408d41eda46c2a43cf2
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-05 21:42:27 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -6,7 +6,7 @@ cart=[
 
 def consolidate_cart(cart)
   consolidatedcart={}
-  cart.each do |item, itemdata|
+  cart.each do |item|
     if consolidatedcart.keys.include?(item)
       puts "Exists"
     else
